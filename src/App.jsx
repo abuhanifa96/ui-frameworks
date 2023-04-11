c58a7f5fe6db9e352c1cd3499b4676655ac072e2
shourovahmd <moh.abuhanifa@gmail.com>
2023-04-11 16:35:57 +0600

    tailwind added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
+    <div >
       home
+      <p className='text-red-700'> hi</p>
     </div>
   )
 }
