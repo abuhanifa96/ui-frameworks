16adbbcc102afa75d01d366d6284010fed9f8fed
shourovahmd <moh.abuhanifa@gmail.com>
2023-04-11 17:11:17 +0600

    selector

--- src/store/useStore.js
+++ src/store/useStore.js
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import store from "./store";
 
-export default function useStore() {
-  const [state, setState] = useState(store.getState());
-  useEffect(() => store.subscribe(setState), []);
+export default function useStore(selector = (state) => state) {
+  const [state, setState] = useState(selector(store.getState()));
+  useEffect(() => store.subscribe((state) => setState(selector(state))), []);
   return state;
 }
