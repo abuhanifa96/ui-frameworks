4e7fa1f2fdac5bb64813b3017bfc3cc57c3cf77a
shourovahmd <moh.abuhanifa@gmail.com>
2023-04-11 16:57:42 +0600

    useStore

--- src/store/store.js
+++ src/store/store.js
@@ -3,7 +3,10 @@ function createStore(initialState) {
   const listeners = new Set();
   return {
     getState: () => currentState,
-    setState: (newState) => (currentState = newState),
+    setState: (newState) => {
+      currentState = newState;
+      listeners.forEach((listener) => listener(currentState));
+    },
     subscribe: (listener) => {
       listeners.add(listener);
       return listeners.delete(listener);
---
+++ src/store/useStore.js
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react";
+import store from "./store";
+
+export default function useStore() {
+  const [state, setState] = useState(store.getState());
+  useEffect(() => {
+    store.subscribe(setState);
+  }, []);
+  return state;
+}
