eb98574e115fff30228e6e0a7574710a9bf8bd9f
mabuhanifa <moh.abuhanifa@gmail.com>
2025-04-03 20:30:12 +0600

    nsui app initialized

---
+++ ns-ui/app/page.tsx
@@ -0,0 +1,11 @@
+import FeaturesSection from "@/components/features-8";
+import HeroSection from "@/components/hero-section";
+
+export default function Home() {
+  return (
+    <div>
+      <HeroSection />
+      <FeaturesSection />
+    </div>
+  );
+}
