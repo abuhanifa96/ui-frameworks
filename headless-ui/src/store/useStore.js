5d22cd2d5aeb7e881dfdc870eb60f23e8812bd5d
mabuhanifa <moh.abuhanifa@gmail.com>
2025-04-04 11:50:46 +0600

    files moved

--- headless-ui/src/store/useStore.js
+++
@@ -1,8 +0,0 @@
-import { useEffect, useState } from "react";
-import store from "./store";
-
-export default function useStore(selector = (state) => state) {
-  const [state, setState] = useState(selector(store.getState()));
-  useEffect(() => store.subscribe((state) => setState(selector(state))), []);
-  return state;
-}
--- radix-ui/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { Navbar } from "./components/Nav";
-
-export default function App() {
-  return (
-    <div className="min-h-screen bg-gray-950">
-      <Navbar />
-    </div>
-  );
-}
