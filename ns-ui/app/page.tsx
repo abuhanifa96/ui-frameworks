4926e36552d14056f7bdbe7bc481a15d19672052
mabuhanifa <moh.abuhanifa@gmail.com>
2025-04-04 11:46:48 +0600

    theme hydration error fixed

--- ns-ui/app/page.tsx
+++ ns-ui/app/page.tsx
@@ -2,6 +2,7 @@ import ContentSection from "@/components/content-2";
 import FeaturesSection from "@/components/features-8";
 import FooterSection from "@/components/footer";
 import HeroSection from "@/components/hero-section";
+import { LogoCloud } from "@/components/logo-cloud";
 import TeamSection from "@/components/team";
 import WallOfLoveSection from "@/components/testimonials";
 
@@ -13,6 +14,7 @@ export default function Home() {
       <WallOfLoveSection />
       <ContentSection />
       <TeamSection />
+      <LogoCloud />
 
       <FooterSection />
     </div>
